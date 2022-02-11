9580324baf8b71be110d1dd0eda6c6734772ece3
ChristopherNoelGoldsmith <ChristopherGoldsmith101@yahoo.com>
2022-02-11 09:48:25 -0800

    Contact Me form completed

---
+++ js/models/contact_me.js
@@ -0,0 +1,13 @@
+/*import { UIController } from './dom.js';
+
+export const contactMe = {
+
+    makeSubject () {
+        const UI = new UIController;
+        const subject = UI.subject;
+        let subjectSend = UIController.subjectSend = subject;
+        console.log(subject)
+        return subjectSend;
+    }
+
+};*/
